44826a6304257615d447452bd13dbaeebcd45790
sainakuo <sayyna_kuo@mail.ru>
2021-11-07 00:03:59 +0300

    gb-hw-2-6 added

---
+++ gb-hw-2-6/gb-hw-2-6.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <regex>
+#include <string>
+
+using namespace std;
+
+ostream& endll(ostream& output) {
+	output << endl << endl;
+	return output;
+}
+
+int main()
+{
+	string str;
+	cmatch result;
+	regex regular("^[0-9]+$");
+	int key = 0;
+
+	cout << "1 task" << endl;
+	cout << "input int: " << endl;
+	do
+	{
+		if (key == 1)
+		{
+			cout << "Error! Input again " << endl;
+		}
+		cin >> str;
+		key = 1;
+
+	} while (!regex_match(str.c_str(), result, regular));
+
+	cout << "2 task" << endl;
+	cout << "check 1" << endll << "check 2" << endl;
+	return 0;
+}
+
